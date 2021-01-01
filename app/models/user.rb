8a5c3137f1eb1311df0ff4d9d0bda99b516d6b6b
AlvaroMorte <alvaromorte8888@gmail.com>
2021-01-01 20:57:53 +0200

    update user_test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,12 @@
 class User < ApplicationRecord::Base
+    before_save { self.email = email.downcase }
     validates :name, presence: true, lenght: {maximum: 50}
-    validates :email, presence: true, lenght: {maximum: 255}
-  has_secure_password
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    
+    validates :email, presence: true, lenght: {maximum: 255},
+                            format: { with: VALID_EMAIL_REGEX },
+                            uniqueness: { case_sensitive: false }
+    
+    has_secure_password
+    validates :password, length: { minimum: 6 }
 end
