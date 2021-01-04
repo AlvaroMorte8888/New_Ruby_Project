681d1ef4b75d016c2496b23178c5b4b87804b9a5
AlvaroMorte <alvaromorte8888@gmail.com>
2021-01-04 22:31:13 +0200

    Errors

--- Gemfile
+++ Gemfile
@@ -56,3 +56,5 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'minitest', group: :test
 gem 'minitest-reporters', group: :test
+
+gem "stimulus_reflex", "~> 3.4"
--- db/migrate/20210104194947_create_users.rb
+++ db/migrate/20210104194947_create_users.rb
@@ -4,7 +4,7 @@ class CreateUsers < ActiveRecord::Migration[6.0]
       t.string :name
       t.string :email
 
-      t.timestamps null: false 
+      t.timestamps null: false
     end
   end
 end
