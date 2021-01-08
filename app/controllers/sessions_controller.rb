7bdb27d7f1b5c2957e18e971b0162b7ae7aa6115
AlvaroMorte <alvaromorte8888@gmail.com>
2021-01-08 20:34:40 +0200

    Тест, выявляющий нежелательное сохранение хэша flash

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,11 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:session][:email].downcase) 
-    if user && user.authenticate(params[:session][:password])
+    if user && user.authenticate(params[:session][:password]) # && - это синтаксис Ruby и обознаечает "и"
       # Осуществить вход пользователя и переадресовать на страницу профиля. 
     else
       # Создать сообщение об ошибке.
+      flash[:danger] = 'Invalid email/password combination' # Изображает ошибку при входе, если ничего не вводить!!
       render 'new' 
     end
   end
