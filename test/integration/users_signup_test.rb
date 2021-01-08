fe8516eaf40d79e7ba67c9047812596128e6b555
AlvaroMorte <alvaromorte8888@gmail.com>
2021-01-08 19:03:23 +0200

    Шаблон для тестирования хэша flash

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -5,10 +5,19 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     test "valid signup information" do 
         get signup_path
         assert_difference 'User.count', 1 do
-            post_via_redirect users_path, user: { name: "Example User",email: "user@example.com", password: "password", password_confirmation: "password" }
+            post_via_redirect users_path, user: { name: "Example User",email: "user@example.com", password: "123456", password_confirmation: "123456" }
         end
         assert_template 'users/show'
         assert_select 'div#<CSS id for error explanation>' 
         assert_select 'div.<CSS class for field with error>'
+    end
+
+    test "valid signup information" do 
+        get signup_path
+        assert_difference 'User.count', 1 do
+            post_via_redirect users_path, user: { name: "Example User", email: "user@example.com",password: "123456", password_confirmation: "123456" }
+        end
+        assert_template 'users/show' 
+        assert_not flash.FILL_IN
     end
 end
