fc8c546b80601529627d4aa35a5536fe636d6796
AlvaroMorte <alvaromorte8888@gmail.com>
2021-01-11 21:26:11 +0200

    Обработка флажка «Запомнить меня»

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password]) # && - это синтаксис Ruby и обознаечает "и"
       # Осуществить вход пользователя и переадресовать на страницу профиля. 
       log_in user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       remember user # входи и запомнинание пользователя
       redirect_to user # форма переадресации
     else
@@ -16,7 +17,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end  
 end
