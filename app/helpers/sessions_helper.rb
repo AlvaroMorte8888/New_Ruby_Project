c42b8841374c62a82c78fa9cbac0d4d5d8c296c9
AlvaroMorte <alvaromorte8888@gmail.com>
2021-01-11 19:27:29 +0200

    Запоминание пользователя

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,6 +4,13 @@ module SessionsHelper
         session[:user_id] = user.id
     end    
 
+    # Запоминает пользователя в постоянном сеансе.
+    def remember(user)
+        user.remember
+        cookies.permanent.signed[:user_id] = user.id 
+        cookies.permanent[:remember_token] = user.remember_token
+    end
+   
     # Возвращает текущего вошедшего пользователя (если есть). 
     def current_user
         @current_user ||= User.find_by(id: session[:user_id]) 
